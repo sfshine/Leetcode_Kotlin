9e48f84b8e8762c35ec4b1b9f3379213ca4a4724
gaoshuai <gaoshuai@teddymobile.cn>
2018-11-06 15:42:08 +0800

    增加LenthOfLastWord

---
+++ src/String/LengthOfLastWord.kt
@@ -0,0 +1,18 @@
+package String
+
+fun main(array: Array<String>) {
+    val test = "Hello World "
+    println("getLengthOfLastWord: ${getLengthOfLastWord(test)}")
+}
+
+fun getLengthOfLastWord(str: String): Int {
+    val length = str.length
+    for (i in length - 1 downTo 0) {
+        val ch = str[i]
+        println(ch)
+        if (ch == ' ') {
+            return length - 1 - i
+        }
+    }
+    return -1
+}
