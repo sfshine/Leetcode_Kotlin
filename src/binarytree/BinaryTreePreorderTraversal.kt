85660792d16efddd1a741729e34ed61a064064fd
gaoshuai09 <gaoshuai09@meituan.com>
2020-02-15 21:07:32 +0800

    增加注释

--- src/binarytree/BinaryTreePreorderTraversal.kt
+++ src/binarytree/BinaryTreePreorderTraversal.kt
@@ -4,14 +4,23 @@ import java.util.*
 
 class BinaryTreePreorderTraversal {
 
-    private class Command(var op: Int, var node: TreeNode) {
-        companion object {
-            const val OP_PRINT = 1
-            const val OP_TRAVERSAL = 2
-        }
-    }
-
+    /**
+     * 前遍历方式:
+     * 使用一个Stack储存临时任务,如果有两种类型:1打印 2访问
+     *
+     * 1.如果是打印命令,则直接把任务中的节点值打印之
+     * 2.如果访问的节点还有子节点,则生成"访问任务"压栈,最后将自己节点生成"打印任务"压栈
+     *
+     * 从stack取出栈顶任务,进行上面的处理,直到stack为空为止
+     */
     class Solution {
+        private class Command(var op: Int, var node: TreeNode) {
+            companion object {
+                const val OP_PRINT = 1
+                const val OP_TRAVERSAL = 2
+            }
+        }
+
         fun preorderTraversal(root: TreeNode?): List<Int> {
             if (root == null) return emptyList()
             var result = mutableListOf<Int>()
--- src/binarytree/InvertBinaryTree.kt
+++ src/binarytree/InvertBinaryTree.kt
@@ -1,6 +1,9 @@
 package binarytree
 
-
+/**
+ * 交给孩子去翻转
+ * 退出条件是孩子没有自己的孩子
+ */
 class InvertBinaryTree {
     class Solution {
         fun invertTree(root: TreeNode?): TreeNode? {
